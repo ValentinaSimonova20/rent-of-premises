027d81ec5dd74f6a5b142a6dccb105294c6f018c
ValentinaSimonova20 <simonova.valentina19@gmail.com>
2020-11-01 17:27:57 +0300

    Initial Project Add

---
+++ src/main/java/simonova/rent/rentofpremises/RentOfPremisesApplication.java
@@ -0,0 +1,13 @@
+package simonova.rent.rentofpremises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentOfPremisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RentOfPremisesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/simonova/rent/rentofpremises/RentOfPremisesApplicationTests.java
@@ -0,0 +1,13 @@
+package simonova.rent.rentofpremises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentOfPremisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
