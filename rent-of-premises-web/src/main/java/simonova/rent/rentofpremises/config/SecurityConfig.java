64b3ed8adc3dd22067a9e1b22decf970e5e058e6
ValentinaSimonova20 <simonova.valentina19@gmail.com>
2020-11-13 23:33:20 +0300

    adding registration for clients. Clients writes to the db

--- rent-of-premises-web/src/main/java/simonova/rent/rentofpremises/config/SecurityConfig.java
+++ rent-of-premises-web/src/main/java/simonova/rent/rentofpremises/config/SecurityConfig.java
@@ -38,7 +38,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/","/register").permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
--- rent-of-premises-web/src/main/java/simonova/rent/rentofpremises/controllers/LoginController.java
+++ rent-of-premises-web/src/main/java/simonova/rent/rentofpremises/controllers/LoginController.java
@@ -6,9 +6,23 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import simonova.rent.rentofpremises.model.Client;
+import simonova.rent.rentofpremises.model.Person;
+import simonova.rent.rentofpremises.model.Role;
+import simonova.rent.rentofpremises.model.Status;
+import simonova.rent.rentofpremises.repositories.ClientRepository;
+
+import java.util.Optional;
 
 @Controller
 public class LoginController {
+
+    ClientRepository clientRepository;
+
+    public LoginController(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
 
     @GetMapping("/login")
     public String login(){
@@ -44,4 +58,36 @@ public class LoginController {
         model.addAttribute("user_name",authentication.getName());
         return "managers/index";
     }
+
+    /**
+     * Отображение страницы регистрации
+     * @return html страница регистрации
+     */
+    @GetMapping("/register")
+    public String getRegisterPage(){
+        return "register/index";
+    }
+
+    /**
+     * Добавить нового клиента в бд
+     * @param newClient
+     * @param model
+     * @return
+     * // TODO: захешировать пароль и так его записать в БД. Убрать Контроллеры регистрации в отдельный файл
+     */
+    @PostMapping("/register")
+    public String addClient(Client newClient, Model model){
+        Optional<Client> client = clientRepository.findByEmail(newClient.getEmail());
+        if(client.isPresent()){
+            model.addAttribute("message","Пользователь с таким email уже зарегистрирован");
+            return "register/index";
+        }
+
+        newClient.setStatus(Status.ACTIVE);
+        newClient.setRole(Role.USER);
+        clientRepository.save(newClient);
+        return "redirect:/login";
+    }
+
+
 }
