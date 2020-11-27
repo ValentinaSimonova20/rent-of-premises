ca6ca79d110278e62a12fee30014606b2546c555
ValentinaSimonova20 <simonova.valentina19@gmail.com>
2020-11-27 22:59:39 +0300

    Adding navigation menu to client pages. Closes #5

--- rent-of-premises-web/src/main/java/simonova/rent/rentofpremises/controllers/UserController.java
+++ rent-of-premises-web/src/main/java/simonova/rent/rentofpremises/controllers/UserController.java
@@ -22,7 +22,7 @@ public class UserController {
      * @return
      */
     @GetMapping("/areas")
-    public String areas(Model model){
+    public String getAreas(Model model){
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String currentPrincipalName = authentication.getName();
@@ -33,5 +33,25 @@ public class UserController {
         model.addAttribute("premises", premisesService.findAll());
 
         return "clients/index";
+    }
+
+    /**
+     * Страница со списком заявок на аренду клиента
+     * @return
+     */
+    @GetMapping("/applications")
+    public String getApplications(Model model){
+        model.addAttribute("activePage","applications");
+        return "clients/applications";
+    }
+
+    /**
+     * Страница c профилем клиента(личной информацией)
+     * @return
+     */
+    @GetMapping("/profile")
+    public String getContracts(Model model){
+        model.addAttribute("activePage","profile");
+        return "clients/profile";
     }
 }
