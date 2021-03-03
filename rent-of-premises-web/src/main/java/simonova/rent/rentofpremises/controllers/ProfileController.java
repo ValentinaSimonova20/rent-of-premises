7074a636a2d400dbe48b2c6fb8edf69504f9622f
ValentinaSimonova20 <simonova.valentina19@gmail.com>
2021-03-03 03:24:57 +0300

    add bootstrap to profile page. add logout link in navigation menu

--- rent-of-premises-web/src/main/java/simonova/rent/rentofpremises/controllers/ProfileController.java
+++ rent-of-premises-web/src/main/java/simonova/rent/rentofpremises/controllers/ProfileController.java
@@ -1,11 +1,15 @@
 package simonova.rent.rentofpremises.controllers;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import simonova.rent.rentofpremises.dto.PremisesDTO;
 import simonova.rent.rentofpremises.dto.UserDTO;
 import simonova.rent.rentofpremises.model.Person;
@@ -18,10 +22,13 @@ import javax.validation.Valid;
  * Контроллер для обработки действий в пункте меню "Ваш профиль"
  */
 
+@Slf4j
 @Controller
+@RequestMapping("/profile")
 public class ProfileController {
 
     private final UserService clientService;
+    private static final String VIEWS_PROFILE_FORM = "clients/profile";
 
     public ProfileController(UserService clientService) {
         this.clientService = clientService;
@@ -31,15 +38,14 @@ public class ProfileController {
      * Отображение страницы c профилем клиента или менеджера(личной информацией)
      * @return html страница с информацией пользователя
      */
-    @GetMapping("/profile")
+    @PreAuthorize("hasAuthority('developers:read')")
+    @GetMapping
     public String getProfile( Model model){
 
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        String currentPrincipalName = authentication.getName();
-
-        model.addAttribute("client", clientService.findByEmail(currentPrincipalName));
-        return "clients/profile";
+        model.addAttribute("client", Person.getAuthUser(authentication, clientService));
+        return VIEWS_PROFILE_FORM;
     }
 
     /**
@@ -47,13 +53,33 @@ public class ProfileController {
      * @param userDTO информация об авторизованном клиенте
      * @return html страница с обновленной информацией пользователя
      */
-    @PostMapping("/profile")
-    public String editProfile(@Valid @ModelAttribute("client") UserDTO userDTO){
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        String currentPrincipalName = authentication.getName();
-        UserDTO currentClient = clientService.findByEmail(currentPrincipalName);
+    @PreAuthorize("hasAuthority('developers:read')")
+    @PostMapping
+    public String editProfile(@Valid @ModelAttribute("client") UserDTO userDTO, BindingResult result, Model model){
 
-        userDTO = currentClient;
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (result.hasErrors()) {
+            result.getAllErrors().forEach(objectError -> {
+                log.debug(objectError.toString());
+            });
+            userDTO.setRole(Person.getAuthUser(authentication, clientService).getRole());
+            model.addAttribute("client", userDTO);
+            return VIEWS_PROFILE_FORM;
+        }
+
+
+        // Проверка на то, нет ли в базе данных пользователя с таким email
+        UserDTO newClient = clientService.findByEmail(userDTO.getEmail());
+        if(newClient != null & !userDTO.getEmail().equals(Person.getAuthUser(authentication, clientService).getEmail())){
+            model.addAttribute("message","Пользователь с таким email уже зарегистрирован");
+            userDTO.setRole(Person.getAuthUser(authentication, clientService).getRole());
+            model.addAttribute("client", userDTO);
+            return  VIEWS_PROFILE_FORM;
+        }
+
+        UserDTO currentClient = Person.getAuthUser(authentication, clientService);
+
         userDTO.setId(currentClient.getId());
         userDTO.setPass(currentClient.getPass());
         userDTO.setStatus(currentClient.getStatus());
@@ -61,7 +87,8 @@ public class ProfileController {
 
         // сохранить изменения в базе данных
         clientService.save(userDTO);
-        return "clients/profile";
+        model.addAttribute("client", userDTO);
+        return VIEWS_PROFILE_FORM;
 
     }
 
